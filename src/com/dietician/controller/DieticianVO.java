d1b913d57efd376f497066c6dd0366f11b614021
PieHuang <b506096094@tmu.edu.tw>
2021-08-23 10:08:21 +0800

    create DieticianVO.java

---
+++ src/com/dietician/controller/DieticianVO.java
@@ -0,0 +1,205 @@
+package com.dietician.controller;
+
+import java.sql.Date;
+
+public class DieticianVO {
+	private Integer dNo;
+	private String dName;
+	private String dAccount;
+	private String dPassword;
+	private Date dBirthDay;
+	private String dPic;
+	private Integer dphone;
+	private String dAddress;
+	private String dEmail;
+	private String edu;
+	private String exp;
+	private String lic;
+	private String prof;
+	private Integer clPrice;
+	private Integer mPrice;
+	private String Intro;
+	private Integer dState;
+	private Integer totalScore;
+	private Integer totalReviewer;
+	private Integer dOnState;
+	private String offDay;
+	private String optTime;
+
+	public Integer getdNo() {
+		return dNo;
+	}
+
+	public void setdNo(Integer dNo) {
+		this.dNo = dNo;
+	}
+
+	public String getdName() {
+		return dName;
+	}
+
+	public void setdName(String dName) {
+		this.dName = dName;
+	}
+
+	public String getdAccount() {
+		return dAccount;
+	}
+
+	public void setdAccount(String dAccount) {
+		this.dAccount = dAccount;
+	}
+
+	public String getdPassword() {
+		return dPassword;
+	}
+
+	public void setdPassword(String dPassword) {
+		this.dPassword = dPassword;
+	}
+
+	public Date getdBirthDay() {
+		return dBirthDay;
+	}
+
+	public void setdBirthDay(Date dBirthDay) {
+		this.dBirthDay = dBirthDay;
+	}
+
+	public String getdPic() {
+		return dPic;
+	}
+
+	public void setdPic(String dPic) {
+		this.dPic = dPic;
+	}
+
+	public Integer getDphone() {
+		return dphone;
+	}
+
+	public void setDphone(Integer dphone) {
+		this.dphone = dphone;
+	}
+
+	public String getdAddress() {
+		return dAddress;
+	}
+
+	public void setdAddress(String dAddress) {
+		this.dAddress = dAddress;
+	}
+
+	public String getdEmail() {
+		return dEmail;
+	}
+
+	public void setdEmail(String dEmail) {
+		this.dEmail = dEmail;
+	}
+
+	public String getEdu() {
+		return edu;
+	}
+
+	public void setEdu(String edu) {
+		this.edu = edu;
+	}
+
+	public String getExp() {
+		return exp;
+	}
+
+	public void setExp(String exp) {
+		this.exp = exp;
+	}
+
+	public String getLic() {
+		return lic;
+	}
+
+	public void setLic(String lic) {
+		this.lic = lic;
+	}
+
+	public String getProf() {
+		return prof;
+	}
+
+	public void setProf(String prof) {
+		this.prof = prof;
+	}
+
+	public Integer getClPrice() {
+		return clPrice;
+	}
+
+	public void setClPrice(Integer clPrice) {
+		this.clPrice = clPrice;
+	}
+
+	public Integer getmPrice() {
+		return mPrice;
+	}
+
+	public void setmPrice(Integer mPrice) {
+		this.mPrice = mPrice;
+	}
+
+	public String getIntro() {
+		return Intro;
+	}
+
+	public void setIntro(String intro) {
+		Intro = intro;
+	}
+
+	public Integer getdState() {
+		return dState;
+	}
+
+	public void setdState(Integer dState) {
+		this.dState = dState;
+	}
+
+	public Integer getTotalScore() {
+		return totalScore;
+	}
+
+	public void setTotalScore(Integer totalScore) {
+		this.totalScore = totalScore;
+	}
+
+	public Integer getTotalReviewer() {
+		return totalReviewer;
+	}
+
+	public void setTotalReviewer(Integer totalReviewer) {
+		this.totalReviewer = totalReviewer;
+	}
+
+	public Integer getdOnState() {
+		return dOnState;
+	}
+
+	public void setdOnState(Integer dOnState) {
+		this.dOnState = dOnState;
+	}
+
+	public String getOffDay() {
+		return offDay;
+	}
+
+	public void setOffDay(String offDay) {
+		this.offDay = offDay;
+	}
+
+	public String getOptTime() {
+		return optTime;
+	}
+
+	public void setOptTime(String optTime) {
+		this.optTime = optTime;
+	}
+
+}
