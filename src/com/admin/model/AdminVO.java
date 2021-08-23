4c5f82762f1528d534b3cb7d2f302fb8c76e3f7e
uni <unique_0713@hotmail.com>
2021-08-23 14:31:15 +0800

    2021/8/23

---
+++ src/com/admin/model/AdminVO.java
@@ -0,0 +1,41 @@
+package com.admin.model;
+
+public class AdminVO {
+	private Integer aNo;
+	private String aName;
+	private String aId;
+	private String aPsw;
+
+	public Integer getaNo() {
+		return aNo;
+	}
+
+	public void setaNo(Integer aNo) {
+		this.aNo = aNo;
+	}
+
+	public String getaName() {
+		return aName;
+	}
+
+	public void setaName(String aName) {
+		this.aName = aName;
+	}
+
+	public String getaId() {
+		return aId;
+	}
+
+	public void setaId(String aId) {
+		this.aId = aId;
+	}
+
+	public String getaPsw() {
+		return aPsw;
+	}
+
+	public void setaPsw(String aPsw) {
+		this.aPsw = aPsw;
+	}
+
+}
