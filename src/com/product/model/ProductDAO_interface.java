706d565f06cee43c666c05bcfd8a64d87735f16e
Wayne Lin <waynelin30078@gmail.com>
2021-08-30 18:24:16 +0800

    解衝突

--- src/com/product/model/ProductDAO_interface.java
+++ src/com/product/model/ProductDAO_interface.java
@@ -3,7 +3,7 @@ package com.product.model;
 import java.util.List;
 
 public interface ProductDAO_interface {
-<<<<<<< HEAD
+
 
 	public void insert(ProductVO productVO);
 
@@ -19,13 +19,4 @@ public interface ProductDAO_interface {
 
 	public List<ProductVO> getAll_byCategoryName(String pName);
 
-=======
-    public void insert(ProductVO prodVO);
-    public void update(ProductVO prodVO);
-    public void delete(Integer pNo);
-    public ProductVO findByPrimaryKey(Integer pNo);
-    public List<ProductVO> getAll();
-    //萬用複合查詢(傳入參數型態Map)(回傳 List)
-//  public List<EmpVO> getAll(Map<String, String[]> map); 
->>>>>>> COURSE
 }
